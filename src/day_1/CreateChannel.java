f577de57b4af6f21c4fa14387b852a65932fee6e
ahmet1972 <62038506+ahmet1972@users.noreply.github.com>
2020-05-12 20:12:07 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,6 +4,8 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("we have a new project");
         System.out.println("update it now");
+         System.out.println("Recent version is now ready on YouTube");
+        
     }
 
 }
