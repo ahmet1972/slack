01bf367bf072e57b5bdd3fc4d8032446cc7dd2b4
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-12 20:04:59 +0100

    add CreateChannel.java and login.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+        System.out.println("we have a new project");
+        System.out.println("update it now");
+    }
+
+}
---
+++ src/day_1/login.java
@@ -0,0 +1,15 @@
+package day_1;
+
+public class login {
+    public static void main(String[] args) {
+        System.out.println("Here is the list of credentials");
+        String name;
+        String password;
+        String recoveryEmail;
+        int grade;
+
+
+    }
+
+
+}
