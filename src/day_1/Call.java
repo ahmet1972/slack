842b006e85701e7c8f9819c891423396344536e8
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-13 17:46:07 +0100

    update Call.java

---
+++ src/day_1/Call.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Call {
+    public static void main(String[] args) {
+        //Users are able to call
+        //branch names should be relevant with the names of user stories for practicality and simplicity
+        //let's add that one only
+    }
+}
